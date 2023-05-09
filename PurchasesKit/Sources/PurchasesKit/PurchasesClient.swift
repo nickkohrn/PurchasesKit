c2a8b3f77d457a9a508b2d021b97053503dc2f65
Nick Kohrn <bnkohrn@icloud.com>
2023-05-09 14:52:45 -0400

    Remove logger

--- PurchasesKit/Sources/PurchasesKit/PurchasesClient.swift
+++ PurchasesKit/Sources/PurchasesKit/PurchasesClient.swift
@@ -1,14 +1,8 @@
 import Foundation
-import OSLog
 import RevenueCat
 import XCTestDynamicOverlay
 
 public struct PurchasesClient {
-    private static let logger = Logger(
-        subsystem: Bundle.main.bundleIdentifier!,
-        category: String(describing: Self.self)
-    )
-
     public var canMakePayments: @Sendable () async -> Bool
     public var configure: @Sendable (_ apiKey: String) async -> Void
     public var products: @Sendable (_ identifiers: Set<String>) async -> [PurchasableProduct]
