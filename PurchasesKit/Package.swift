df00d2a0059f4925014c85055d1d0f98d9a76851
Nick Kohrn <bnkohrn@icloud.com>
2023-05-09 09:11:12 -0400

    Add dependencies

--- PurchasesKit/Package.swift
+++ PurchasesKit/Package.swift
@@ -6,21 +6,23 @@ import PackageDescription
 let package = Package(
     name: "PurchasesKit",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PurchasesKit",
             targets: ["PurchasesKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", .upToNextMajor(from: "0.0.0")),
+        .package(url: "https://github.com/RevenueCat/purchases-ios.git", .upToNextMajor(from: "4.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PurchasesKit",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "RevenueCat", package: "purchases-ios"),
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "PurchasesKitTests",
             dependencies: ["PurchasesKit"]),
